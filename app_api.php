c76ab7f81bde309ad54ed4a37d73555ab7a9e350
afify <hassan@afify.dev>
2021-03-18 17:57:01 +0300

    add maakbot api php

---
+++ app_api.php
@@ -0,0 +1,121 @@
+<?php
+// https://codeofaninja.com/2018/09/rest-api-authentication-example-php-jwt-tutorial.html
+// required headers
+header("Access-Control-Allow-Origin: http://localhost/rest-api-authentication-example/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ // check if given email exist in the database
+
+ function emailExists(){
+ 
+    // query to check if email exists
+    $query = "SELECT id, firstname, lastname, password
+            FROM " . $this->table_name . "
+            WHERE email = ?
+            LIMIT 0,1";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare( $query );
+ 
+    // sanitize
+    $this->email=htmlspecialchars(strip_tags($this->email));
+ 
+    // bind given email value
+    $stmt->bindParam(1, $this->email);
+ 
+    // execute the query
+    $stmt->execute();
+ 
+    // get number of rows
+    $num = $stmt->rowCount();
+ 
+    // if email exists, assign values to object properties for easy access and use for php sessions
+    if($num>0){
+ 
+        // get record details / values
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+        // assign values to object properties
+        $this->id = $row['id'];
+        $this->firstname = $row['firstname'];
+        $this->lastname = $row['lastname'];
+        $this->password = $row['password'];
+ 
+        // return true because email exists in the database
+        return true;
+    }
+ 
+    // return false if email does not exist in the database
+    return false;
+}
+
+// files needed to connect to database
+include_once 'config/database.php';
+include_once 'objects/user.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// instantiate user object
+$user = new User($db);
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+ 
+// set product property values
+$user->email = $data->email;
+$email_exists = $user->emailExists();
+ 
+// files for jwt will be here
+// generate json web token
+include_once 'config/core.php';
+include_once 'libs/php-jwt-master/src/BeforeValidException.php';
+include_once 'libs/php-jwt-master/src/ExpiredException.php';
+include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once 'libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+ 
+// generate jwt will be here
+// check if email exists and if password is correct
+if($email_exists && password_verify($data->password, $user->password)){
+ 
+    $token = array(
+       "iat" => $issued_at,
+       "exp" => $expiration_time,
+       "iss" => $issuer,
+       "data" => array(
+           "id" => $user->id,
+           "firstname" => $user->firstname,
+           "lastname" => $user->lastname,
+           "email" => $user->email
+       )
+    );
+ 
+    // set response code
+    http_response_code(200);
+ 
+    // generate jwt
+    $jwt = JWT::encode($token, $key);
+    echo json_encode(
+            array(
+                "message" => "Successful login.",
+                "jwt" => $jwt
+            )
+        );
+ 
+}
+ 
+// login failed will be here
+// login failed
+else{
+ 
+    // set response code
+    http_response_code(401);
+ 
+    // tell the user login failed
+    echo json_encode(array("message" => "Login failed."));
+}
+?>
